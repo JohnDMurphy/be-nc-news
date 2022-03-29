d473e2edee60d04c117388d2b7ec60a76d3fafb4
John Murphy <jd031988@gmail.com>
2022-03-29 16:33:57 +0100

    initial setup for get users

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/api/topics', getTopics);
 
 app.get('/api/articles/:article_id', getArticleById);
 
-app.get('/api/users', getUsers);
+// app.get('/api/users', getUsers);
 
 app.all('*', (req, res) => {
   res.status(404).send({ msg: 'Route not found!' });
